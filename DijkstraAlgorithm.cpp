ecc9e855a2a4e6517f35c6a5737860f43d71ec2f
Himanshu Aggarwal <31725055+AggHim@users.noreply.github.com>
2019-06-13 22:42:12 +0530

    Update DijkstraAlgorithm.cpp

--- DijkstraAlgorithm.cpp
+++ DijkstraAlgorithm.cpp
@@ -1,7 +1,7 @@
 /*
 Initially weights for all vertices will be marked as Infinity, now there will be a visited array to keep track
 of the vertices that are visited. We chose vertex with minimum weight from the unvisited and explore all it's neighbours
-that are also unvisited and update their weights and parents is distance is lower than current weight.
+that are also unvisited and update their weights and parents if distance is lower than current weight.
 */
 
 #include <iostream>
